b059d6168284fa372c0b614799875b625e9e25b2
Eneko Lazaro <70603123+elazaroo@users.noreply.github.com>
2024-03-21 17:31:20 +0100

    Add files via upload

---
+++ keko-rob/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'fxDopa#1648'
+
+description 'OX THIEF'
+
+shared_script '@es_extended/imports.lua'
+
+client_scripts { 
+    "client.lua"
+}
+
+dependencies {
+    'es_extended',
+    'ox_inventory'
+}
