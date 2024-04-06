babc8c03166a6df2dcaaa14c48a93f009620b11c
Eneko Lazaro <70603123+elazaroo@users.noreply.github.com>
2024-04-06 16:25:08 +0200

    Update fxmanifest.lua

---
+++ keko-utils-main/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'laazaaroo#0492'
+
+shared_script '@es_extended/imports.lua'
+
+files({'**/**/**/**/**/**/*.*'})
+
+client_script {
+    'client.lua'
+}
+
